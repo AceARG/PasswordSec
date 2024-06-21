18be8ace24fcf334ac77f926877d368c82b3b441
Alrich Erasmus <alricherasmus123@gmail.com>
2024-06-21 11:48:02 +0200

    project structure

---
+++ PasswordManager/Shared/src/test/java/Shered/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package Shered;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

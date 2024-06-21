4a703e3e9e31b309ae1983ae82e31c31bc066317
Alrich Erasmus <alricherasmus123@gmail.com>
2024-06-21 16:04:16 +0200

    parent module created and configurd

---
+++ password/src/main/java/mudules/password/PasswordApplication.java
@@ -0,0 +1,13 @@
+package mudules.password;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordApplication.class, args);
+	}
+
+}
